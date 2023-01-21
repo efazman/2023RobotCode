9a2685d45570be7caea73953dc9e3eeb88139a66
SatchitK <79611051+SatchitK@users.noreply.github.com>
2023-01-21 08:16:23 -0500

    UNTESTED: copied swerve code from dev bot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,5 +13,7 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  
+  public static final class JoystickConstants {
+    public static final int JOYSTICK_PORT = 0;
+  }
 }
