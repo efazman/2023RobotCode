565298287a8c828e938149b8ce4817f263285d5e
SatchitK <79611051+SatchitK@users.noreply.github.com>
2023-01-26 09:21:54 -0500

    restructured 2019 IO classes into CrevoLib

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 
 
+import org.photonvision.PhotonCamera;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -14,27 +16,33 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.JoystickConstants;
 import frc.robot.Constants.SwerveDrivetrainConstants;
+import frc.robot.Constants.VisionConstants;
 import frc.robot.commands.TeleopDrive;
+import frc.robot.subsystems.PoseEstimator;
 import frc.robot.subsystems.SwerveDrivetrain;
 
 
 public class RobotContainer {
 
-
+  /*Declare External Sensors -> Cameras */
+  private final PhotonCamera m_Limelight = new PhotonCamera(VisionConstants.CAMERA_NAME);
   /*Declare Joystick*/
   private final XboxController m_driverController = new XboxController(JoystickConstants.DRIVER_PORT_ID);
   
   /*Declare Subsystems*/
   private final SwerveDrivetrain m_swerveDrivetrain = new SwerveDrivetrain();
+  private final PoseEstimator m_poseEstimator = new PoseEstimator(m_Limelight, m_swerveDrivetrain);
 
   /*Map Joystick Axis and Functions*/
   private final int m_translationAxis = XboxController.Axis.kLeftY.value;
   private final int m_strafeAxis = XboxController.Axis.kLeftX.value;
   private final int m_rotationAxis = XboxController.Axis.kRightX.value;
+  
 
   /*Map Joystick Buttons and Functions*/
   private final JoystickButton m_zeroGyro = new JoystickButton(m_driverController, XboxController.Button.kY.value);
-  
+  private final JoystickButton m_resetRobotFieldPose = new JoystickButton(m_driverController, XboxController.Button.kA.value);
+
   public RobotContainer() {
     
     m_swerveDrivetrain.setDefaultCommand(new TeleopDrive(m_swerveDrivetrain, 
@@ -56,7 +64,11 @@ public class RobotContainer {
   private void configureBindings() {
     /*Define onPressed Commands for Joystick Buttons and Triggers*/
 
-    m_zeroGyro.onTrue(new InstantCommand(() -> m_swerveDrivetrain.resetGyro()));
+    m_zeroGyro.onTrue(
+      new InstantCommand(() -> m_swerveDrivetrain.resetGyro()));
+      
+    m_resetRobotFieldPose.onTrue(
+      new InstantCommand(() -> m_poseEstimator.resetFieldPosition()));
 
     
     
--- src/main/java/frc/robot/io/MissingIoObjectException.java
+++
@@ -1,4 +0,0 @@
-package frc.robot.io;
-
-public class MissingIoObjectException extends Exception {
-}
